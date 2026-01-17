11310de612148241898438b3830fc6ae5aff0709
ingenarel (NeoJesus) <ingenarel_neojesus@disroot.org>
2026-01-18 03:01:46 +0600

    feat(refactor): refactor the script and update ci

--- gentoo-version-check.sh
+++ gentoo-version-check.sh
@@ -2,157 +2,81 @@
 
 [ -z "$GH_TOKEN" ] && {
     echo "No valid github token"
-    exit 1
+    gh auth status > /dev/null 2>&1 || {
+        echo "gh cli isn't authenticated"
+        exit
+    }
 }
 
-git clone --single-branch --depth 1 --branch dev https://github.com/gentoo/guru.git > /dev/null || git -C ./guru pull > /dev/null
-# git clone --single-branch --depth 1 --branch dev https://github.com/gentoo/gentoo.git > /dev/null || git -C ./gentoo pull > /dev/null
+scriptDir="$(realpath --canonicalize-missing "$0/..")"
 
-printTable() {
-    echo "| Package | Downstream | Upstream | Maintainer |"
-    echo "| --- | --- | --- | --- |"
+while IFS='' read -r line; do
+    repoName="$(echo "$line" | awk '{print $1}')"
+    repoBranch="$(echo "$line" | awk '{print $2}')"
+    repoLink="$(echo "$line" | awk '{print $3}').git"
+    git\
+        clone\
+        --single-branch\
+        --depth 1\
+        --branch "$repoBranch"\
+        "$repoLink"\
+        "$scriptDir/$repoName" > /dev/null ||
+    git -C "$scriptDir/$repoName" pull > /dev/null
+    [ "$1" != dry ] && "$scriptDir/checkgithubfiles.sh" "$scriptDir/$repoName"
+done < "$scriptDir/config"
 
-    find "$1" -maxdepth 2 -type d -regextype posix-extended -regex '.*/.+/.+' | while IFS='' read -r packageName; do
-        remoteLine="$(grep '<remote-id type="github">' "$packageName/metadata.xml" 2>/dev/null)"
-        [ -n "$remoteLine" ] && {
-            properRemote="$(
-            echo "$remoteLine"\
-                |
-            sed -E 's/^.*<remote-id\s*type="github">\s*(\S+\/\S+)\s*<\/remote-id>.*$/\1/' 2>/dev/null\
-                |
-            head -n1
-        )"
-            find "$packageName" -maxdepth 1 -type f -regex '.*\.ebuild' | sort -r | while IFS='' read -r ebuildName; do
-                echo "$ebuildName" | grep -q '9999' && {
-                    continue
-                }
-                actualPackage="$(dirname "$ebuildName" | grep -oE '[^/]+$')"
-                actualEbuild="$(basename -s ".ebuild" "$ebuildName")"
-                actualVersion="$(echo "$actualEbuild" | sed -E "s/$actualPackage-([^_\-]+).*/\1/" )"
-                # using curl should be faster but gh should be better
-                latestVersion="$(
-                gh release --repo "$properRemote" list --limit 2 --order desc --json tagName --jq '.[].tagName'\
-                    |
-                    sed -E '/(nightly|continuous|test-build|weekly|latest|current)/d'\
-                    |
-                head -n1\
-                    |
-                sed -E 's/v?(.+)/\1/'
-                # curl --silent -L -H "Accept: application/vnd.github+json"\
-                #     -H "Authorization: Bearer $GH_TOKEN"\
-                #     -H "X-GitHub-Api-Version: 2022-11-28"\
-                #     "https://api.github.com/repos/$properRemote/tags?per_page=2&page=1"\
-                #     |
-                # sed -nE '/nightly/d; s/\s*"name":\s*"v?(\S+)".*/\1/p'\
-                #     |
-                # head -n1
-            )"
+readMeData="$( cat "$scriptDir/README.md" )"
+rm "$scriptDir/README.md"
+tableHasStarted=false
+echo "$readMeData" | while IFS='' read -r line; do
+    case "$line" in
+        '<!-- start -->')
+            tableHasStarted=true
+            printf "%s\n" "$line" >> "$scriptDir/README.md"
+            while IFS='' read -r line; do
+                repoName="$(echo "$line" | awk '{print $1}')"
+                repoBranch="$(echo "$line" | awk '{print $2}')"
+                repoLink="$(echo "$line" | sed -n -E 's/.*\s+(\S+)$/\1/p')"
                 {
-                    [ -n "$latestVersion" ]\
-                        &&
-                    ! printf '%s\n%s\n' "$latestVersion" "$actualVersion"\
-                        |
-                    sort --check=quiet --version-sort
-                    } && {
-                    newPackageName="$(echo "$packageName" | grep -oE '[^/]+/[^/]+$')"
-                    printf "| [%s](https://github.com/gentoo/guru/tree/dev/%s) |" "$newPackageName" "$newPackageName"
-                    printf " %s |" "$actualVersion"
-                    printf " [%s](https://github.com/%s/tags) |" "$latestVersion" "$properRemote"
-                    maintainer=""
-                    if grep -q -E 'maintainer.*needed' "$packageName/metadata.xml"; then
-                        maintainer="none"
-                    else
-                        while IFS='' read -r metaDataMaintainerSearchLine; do
-                            case $metaDataMaintainerSearchLine in
-                                *'<maintainer'*)
-                                    maintainerTagFound=1
-                                    ;;
-                                *'</maintainer>'*)
-                                    maintainerTagFound=0
-                                    ;;
-                                *'<email>'*'</email>'*)
-                                    if [ "$maintainerTagFound" -eq 1 ]; then
-                                        maintainer="$metaDataMaintainerSearchLine"
-                                        maintainer="${maintainer#*<email>}"
-                                        maintainer="${maintainer%</email>*}"
-                                        break
-                                    fi
-                                    ;;
-                            esac
-                        done < "$packageName/metadata.xml"
-                    fi
-                    # printf " | [%s](%s) |\n" "$maintainer" "https://repology.org/maintainer/$(echo "$maintainer" | sed -E '')"
-                    if [ "$maintainer" = "none" ]; then
-                        printf " none |\n"
-                    else
-                        printf " [%s](%s) |\n" "$maintainer" "https://repology.org/maintainer/$maintainer"
-                    fi
-                }
-                break
-            done
-        }
-    done
-}
-
-printf "# Disclaimer:
-
-I have a problem, when I see outdated packages, I want to update them as soon as I can
-
-I tried to find solutions, and in turn also founded like minded people with the same question, and even some answers,
-but none of them seemed suitable for me, and making something myself is always more flexible then trying to find other
-solutions that may not even fulfill my full use case
-
-You may ask why use this when stuff like [repology](https://repology.org/projects/?inrepo=gentoo_ovl_guru&outdated=1)
-exists? it's because this version checks ::guru's dev branch, which is easier for contributors because some package may
-already be updated in the dev branch, and hasn't been updated to the master branch, so repology doesn't track that.
-
-on top of that repology only compares the versions in all package repositories and treats the latest updated version as
-the 'latest build', so what it technically shows as the 'latest version' can actually be outdated from the upstream, to
-explain in simpler terms: if upstream has version 3, arch has version 2, and guru has version 2, repology will show that
-guru is up to date, because it's just comparing betweent the different different versions. but this repo doesn't do it,
-it tries to track upstream and get the latest version data, hence so many api calls.
-
-while some might say that this level of data is not needed, i say that it is needed since just by glancing at the data
-you can guess how many versions is it behind
-
- - Only github is supported for now, will probably add more source checkers later
-
- - Currently it will check ::guru so i don't burn through my github api calls in a single run, if you want a report
- generated for ::gentoo, you should self host it/fork the repo and run the actions manually when you want
-
- - The report might be outdated, because the github actions will only run once per X ammount of time, the time is
- subject to change depending on my own API usage, so you may want to self run this if you want the complete up to date
- version.
-
- - Currently it's not modular, but I think I might be able to make this a usable github action for other repos, if you
- want this, hit me up and I maybe will make one, (no promises :3, and if i don't, you can do it yourself)
-
-### Please:
- - Don't use this repo to make low quality/broken version bumps and/or prs and/or spam bug reports, the whole point is
- to upgrade overlay qualities, and make the lives for package maintainers' and contributors' a bit easier
-
- - If you're a new contributor trying to find packages to bump, [devmanual](https://devmanual.gentoo.org/) is your
- bible, [pkgcheck](https://packages.gentoo.org/packages/dev-util/pkgcheck) scan is your new best friend, and try to join
- the IRC if you can to get help and ask questions to your fellow gentoo mates
-
-### Self host/Fork Host:
-
- - if you want to run this locally, you need to set a GH_TOKEN env var, i recommend using a [fine grained access
- token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#fine-grained-personal-access-tokens)
-
- please remember that the output is designed to be put inside a github readme, so it may not be pretty in the terminal
-
- - if you want to self run this using github actions (i recommend this way), you *should* be just able to fork it,
- create token and set GH_TOKEN secret, then run it through github actions
-
-# [Guru Dev Branch](https://github.com/gentoo/guru/tree/dev):\n\n"
-printTable "$(realpath ./guru)"
-# printf "\n\n"
-# printf "# [Gentoo Master Branch](https://github.com/gentoo/gentoo/tree/dev):\n\n"
-# printTable "$(realpath ./guru)"
-
-printf "\n\n\n\n\n\n<sub>Congrats, you made it to the bottom</sub>\n
-<sub>if you can afford to pay my wifi bills, you can support me in patreon: https://www.patreon.com/IngenarelNeoJesus</sub>\n
-<sub>i don't like putting this in my projects, but until my dropout ass finds a way to get a job/proper funding, i have
-no choice but to accept donations, which i personally don't want to trust me</sub>\n
-"
+                    echo "# [$repoName $repoBranch branch]($repoLink)"
+                    echo "| Package | Downstream | Upstream | Maintainer |"
+                    echo "| ------- | ---------- | -------- | ---------- |"
+                } >> "$scriptDir/README.md"
+                while IFS=',' read -r\
+                    package\
+                    packageLink\
+                    ebuildVersion\
+                    sourceVersion\
+                    sourceTagsLink\
+                    maintainerEmail\
+                    repologyEmail
+                do
+                    printf "| %s | %s | %s | %s |\n"\
+                        "[$package]($packageLink)"\
+                        "$ebuildVersion"\
+                        "[$sourceVersion]($sourceTagsLink)"\
+                        "[$maintainerEmail]($repologyEmail)"
+                done < "$scriptDir/.github-raw-data.csv" |
+                sort >> "$scriptDir/README.md"
+            done < "$scriptDir/config"
+            # {
+            #     head -n1 "$scriptDir/.github-raw-data.csv"
+            #     head -n1 "$scriptDir/.github-raw-data.csv" | tr '[:alnum:]' '-'
+            #     for type in\
+                #         github
+            #     do
+            #         [ -f "$scriptDir/.$type-raw-data.csv" ] && {
+            #             tail -n +2 "$scriptDir/.$type-raw-data.csv" | sort
+            #         }
+            #     done
+            # } | sed -e 's/,/ | /g; s/.*/| & |/' >> "$scriptDir/README.md"
+            ;;
+        '<!-- end -->')
+            tableHasStarted=false
+            printf "%s\n" "$line" >> "$scriptDir/README.md"
+            ;;
+        *)
+            $tableHasStarted || printf "%s\n" "$line" >> "$scriptDir/README.md"
+            ;;
+    esac
+done
